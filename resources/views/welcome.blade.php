52e52e8908cadcda9dc45c9a578310a1d61879dd
Hulk-Tron <creative_15@live.com>
2017-05-27 02:10:41 +0500

    Laravel with React BoilerPlate

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!-- <!doctype html>
+<html lang="{{ config('app.locale') }}"> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>React Boiler Plate</title>
+  <!-- Bootstrap Core CSS -->
+  <link href="https://bootswatch.com/cosmo/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+<script src="{{url('')}}/js/bundle.js"></script>
+</body>
+</html>
+<!-- </html> -->
